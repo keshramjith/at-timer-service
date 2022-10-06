97034b8d75af81011a06461858b9b60c95fbe79e
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-10-06 18:33:47 +0200

    init

---
+++ src/main/java/com/activitytimer/timer/AtTimerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.activitytimer.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtTimerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtTimerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/activitytimer/timer/AtTimerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitytimer.timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtTimerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
